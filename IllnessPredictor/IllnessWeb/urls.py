25e308a365fb433ed868e6c96638c49d7bfd2da1
Borimir <borimir.ganchev@gmail.com>
2023-05-26 09:54:30 +0300

    so far

--- IllnessPredictor/IllnessWeb/urls.py
+++ IllnessPredictor/IllnessWeb/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path 
 from django.contrib import admin
 
-from IllnessWeb.views import index
+from IllnessWeb.views import index, search
 
 urlpatterns = [
     path('', index, name= 'index'),
+    path('search/', search, name= 'search'),
     path('admin/', admin.site.urls),
 ]
