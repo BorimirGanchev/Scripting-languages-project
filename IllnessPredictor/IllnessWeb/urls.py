d06538703dc825acf3dca2ee0e23ef384a3726c5
Borimir <borimir.ganchev@gmail.com>
2023-05-27 16:07:34 +0300

    home page and log in are done

--- IllnessPredictor/IllnessWeb/urls.py
+++ IllnessPredictor/IllnessWeb/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
+from .forms import LoginForm
 
 app_name = 'IllnessWeb'
 
@@ -8,4 +10,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('search/', views.search, name= 'search'),
     path('signup/', views.signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='IllnessWeb/login.html', authentication_form=LoginForm), name= 'login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='IllnessWeb/logout.html'), name= 'logout'),
 ]
--- IllnessPredictor/IllnessWeb/views.py
+++ IllnessPredictor/IllnessWeb/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from .forms import LoginForm
 
 from .forms import SignupForm
 
